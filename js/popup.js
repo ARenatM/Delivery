2cff50020d5c610d98efc01af92b09bcb5ec80f8
A_R <44033096+AMRenat@users.noreply.github.com>
2020-10-19 17:07:19 +0300

    add adaptive popup

---
+++ js/popup.js
@@ -0,0 +1,46 @@
+console.log("1");
+
+const popup = document.querySelector(".popup");
+const body = document.querySelector("body");
+const timeout = 500;
+
+var isOpen = false;
+
+var openPopup = (e) => {
+  if (popup) {
+    popup.classList.add("open");
+  }
+
+  bodyLock(e);
+  isOpen = true;
+};
+
+var closePopup = (e) => {
+  if (isOpen) {
+    popup.classList.remove("open");
+    bodyUnlock(e);
+    isOpen = false;
+  }
+};
+
+function bodyLock(e) {
+  let bodyPaddingValue =
+    window.innerWidth - document.querySelector("main").offsetWidth + "px";
+  body.style.paddingRight = bodyPaddingValue;
+  body.classList.add("lock");
+}
+
+function bodyUnlock(e) {
+  setTimeout(() => {
+    body.style.paddingRight = 0;
+    body.classList.remove("lock");
+  }, timeout);
+}
+
+document.querySelector(".popup__body").addEventListener("click", (e) => {
+  if (isOpen) {
+    if (!e.target.closest(".popup__content")) {
+      closePopup(e);
+    }
+  }
+});
