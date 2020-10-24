b9b735666335910de956164d04a3dc43f57b0b9b
A_R <44033096+AMRenat@users.noreply.github.com>
2020-10-24 11:47:32 +0300

    add coments and optimization

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
+/* Просто получаею все нужные элементы и засовываю в переменные */
 const popup = document.querySelector(".popup");
 const popupBody = document.querySelector(".popup__body");
 const popupList = document.querySelector(".popup__shopping-list");
 const popupCost = document.querySelector(".popup__cost");
 const body = document.querySelector("body");
 const shoppingHeader = document.querySelector(".header__shopping-count");
+
+/* Это для открытия модалки */
 const timeout = 500;
 
-console.log(shoppingHeader);
-
+/* Создаю массив продуктов в корзине */
 var shopping = [];
 
+/* Функция которая добавляет объект в массив */
 var inShopping = (e) => {
+  /* Получаю название продукта и его стоимость */
   let nameProduct = e.target.closest(".card-inner").querySelector(".card-title").outerText;
   let costProduct = e.target.closest(".purchase").querySelector(".restaurant-price").outerText;
+  /* Флаг для проверки существования этого продукта в корзине  */
   let exist = false;
 
   //Проверка существования массива
@@ -30,11 +35,11 @@ var inShopping = (e) => {
         cost: costProduct,
         count: 1,
       });
+      /* Подсчёт общей стоимости */
       calculateCost();
+      /* Отрисовка корзины на сайт */
       shoppingToPopup();
     }
-
-    shoppingHeader.innerText = shopping.length;
   }
   //Если массив пустой добавить
   else {
@@ -46,13 +51,74 @@ var inShopping = (e) => {
     calculateCost();
     shoppingToPopup();
   }
+  /* Открыть модальное окно  */
   openPopup(e);
-  shoppingHeader.innerText = shopping.length;
 };
 
+/* Увеличение */
+var gainCount = (e) => {
+  /* Получаю имя продукта */
+  let name = e.target.closest(".popup__shopping-list__item").querySelector(".shopping-list__name")
+    .outerText;
+  /* нахожу этот продукт в корзине */
+  shopping.forEach((element, index) => {
+    if (element.name === name) {
+      /* если такой продукт в корзине есть то увеличиваю их число на один */
+      shopping[index].count += 1;
+      /* Записываю это на страницу */
+      e.target.previousElementSibling.innerText = shopping[index].count;
+    }
+  });
+  /* Пересчёт общей стоимости */
+  popupCost.innerText = calculateCost() + " P";
+};
+
+/* Уменьшение */
+var reduceCount = (e) => {
+  /* Получаю имя продукта */
+  let name = e.target.closest(".popup__shopping-list__item").querySelector(".shopping-list__name")
+    .outerText;
+  /* Нахожу этот продукт в корзине */
+  shopping.forEach((element, index) => {
+    if (element.name === name) {
+      /* Если такой продукт существует */
+      if (shopping[index].count === 1) {
+        /* Если его количество равно единице то я его удаляю из корзины */
+        shopping.splice(index, 1);
+        /* Перерисовываю всю корзину */
+        shoppingToPopup();
+        /* Записываю сколько осталось продуктов в корзине в шапку  */
+        shoppingHeader.innerText = shopping.length;
+      } else {
+        /* если не 1 то уменьшаю на один и записываю на сайт */
+        shopping[index].count -= 1;
+        e.target.nextElementSibling.innerText = shopping[index].count;
+      }
+    }
+  });
+  /* Пересчёт общей стоимости */
+  popupCost.innerText = calculateCost() + " P";
+};
+
+/* Обнуление корзины */
+var cancel = (e) => {
+  /* Убираю всё из корзины */
+  shopping = [];
+  /* Записываю в шапку что у нас нет в корзине ничего */
+  shoppingHeader.innerText = shopping.length;
+  /* Закрываю модалку */
+  closePopup();
+  /* Через время анимации перерисовываю корзину */
+  setTimeout(() => {
+    shoppingToPopup();
+  }, timeout);
+};
+
+/* Функция перевода корзины из объекта в HTML */
 function shoppingToHTML() {
   let shoppingHTML = "";
   if (shopping.length > 0) {
+    shoppingHeader.innerText = shopping.length;
     shopping.forEach((element) => {
       shoppingHTML += `
     <li class="popup__shopping-list__item">
@@ -72,6 +138,7 @@ function shoppingToHTML() {
   return shoppingHTML;
 }
 
+/* Функция подсчёта общей стоимости */
 function calculateCost() {
   let cost = 0;
 
@@ -82,6 +149,7 @@ function calculateCost() {
   return cost;
 }
 
+/* Функция отрисовки корзины */
 function shoppingToPopup() {
   popupList.innerHTML = shoppingToHTML(shopping);
   popupCost.innerText = calculateCost() + " P";
